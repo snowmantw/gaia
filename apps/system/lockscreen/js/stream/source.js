a07d6a8fb93896a7174c0fc30f7e04c79688ac09
Greg Weng <snowmantw@gmail.com>
2014-11-01 15:30:58 +0800

    Basic Stream & Source is ready

---
+++ apps/system/lockscreen/js/stream/source.js
@@ -0,0 +1,131 @@
+ 'use strict';
+
+/**
+ * The source of Stream. It provide multiple adapter interfaces to build sources
+ * of Stream from various native APIs.
+ *
+ * Source would call stream's 'notify' method to generate the stream implicitly,
+ * and every Stream should contain one or more Source.
+ **/
+(function(exports) {
+  var Source = function() {
+    // Delay the timimg to emit the elements,
+    // since when we get the source, the collector
+    // may stay undefined.
+    this.collectorResolver = null;
+    this.promisedCollector = new Promise((resolve) => {
+      this.collectorResolver = resolve;
+    });
+
+    // Keep them here for debugging.
+    this.iterator =
+    this.events =
+    this.settings =
+    this.timers = null;
+
+    // We need to use setInterval trick for infinite iterator.
+    // See: 'Source.interator'
+    this.iterationID = null;
+  };
+
+  /**
+   * From any iterator. The thing could be finite or infinite.
+   * This means the thing must implement the 'iterator protocol' in ES6.
+   *
+   * Note: we can't use any plain loop to iterate the iterator, since if it's
+   * infinite, we would block main thread. So we can only use setInterval trick
+   * to make sure we can iterate both kinds of iterators.
+   */
+  Source.iterator = function(iterator) {
+    var source = new Source();
+    source.iterator = iterator;
+    source.promisedCollector.then(() => {
+      source.iteration = window.setInterval(() => {
+        var current = source.iterator.next();
+        if (current.done) {
+          window.clearInterval(source.iterationID);
+        }
+        source.emit(current.value);
+      }, 4);  // 4ms is the minimum according to HTML5#setTimeout.
+    });
+    return source;
+  };
+
+  /**
+   * A specialized version of 'Source.iterator', which avoids the 'setInterval'
+   * trick, since an array is always finite, so we can use only the plain loop.
+   */
+  Source.array = function(array) {
+    var source = new Source();
+    source.iterator = array;
+    source.promisedCollector.then(() => {
+      array.forEach((item) => {
+        source.emit(item);
+      });
+      // Let source know we're done, and this is a finite source.
+      source.terminator();
+    });
+    return source;
+  };
+
+  /**
+   * Give a list of pair of event names and targets.
+   * Targets is optional, since most of events can be received via binding
+   * on the global 'window' object.
+   *
+   * So the format is:
+   *
+   *    [{ type: eventType, target: targetDOM }]
+   */
+  Source.events = function(events) {
+    var source = new Source();
+    source.promisedCollector.then(() => {
+      events.forEach((pair) => {
+        var {type, target} = pair;
+        if (!target) {
+          target = window;
+        }
+        target.addEventListener(type, source);
+      });
+    });
+    return source;
+  };
+
+  /**
+   * Listen changes from mozSettings API.
+   */
+  Source.settings = function(keys) {
+    var source = new Source();
+    source.promisedCollector.then(() => {
+      keys.forEach((key) => {
+        navigator.mozSettings.addObserver(key,
+          source.emit.bind(source));
+      });
+    });
+    return source;
+  };
+
+  /**
+   * Set the stream in entry to let source emit the new value.
+   * The first argument is the method to collect emitted item,
+   * and the second one is the method to signal the end of emitting.
+   */
+  Source.prototype.collector = function(collector, terminator) {
+    this.collector = collector;
+    this.collectorResolver(collector);
+    this.terminator = terminator;
+    return this;
+  };
+
+  /**
+   * Emit the value to Stream when it's ready.
+   * Need keep 'handleEvent' since event API relies on it.
+   */
+  Source.prototype.handleEvent =
+  Source.prototype.emit = function(item) {
+    this.collector(item);
+  };
+
+  exports.Source = Source;
+})(window);
+
---
+++ apps/system/lockscreen/js/stream/stream.js
@@ -0,0 +1,92 @@
+ 'use strict';
+ /* global Promise */
+
+/**
+ * Reactive stream to manipulate the future.
+ * Every manipulation would generate a new Stream.
+ **/
+(function(exports) {
+  var Stream = function() {
+    this.sources = [];
+    this.observers = [];
+    this.reducer = null;
+    this.reducingDone = null;
+    this.accumulatedValue = null;
+  };
+
+  /**
+   * Register a source to continue the Stream.
+   */
+  Stream.prototype.source = function(source) {
+    this.sources.push(source.collector(
+      this.notify.bind(this)));
+    return this;
+  };
+
+  /**
+   * The reducer function is not the same with ES5.1's, since we don't
+   * provide the 'index' and 'array' argument, but only the 'previousValue'
+   * and 'currentValue'.
+   *
+   * And since Stream may be an infinite Stream, try to reduce and use it
+   * would never get result (since the reducing time is infinite). This is
+   * true for every attempts that try to 'get and use value' via the reducer.
+   * Instead of, user should treat this function as the 'generator' of the
+   * new notifiable object, which would be implicitly notified everytime the
+   * parent Stream get triggered, and generate the new value. A proper way
+   * to handle this new value is to embed it as another Stream's new element.
+   *
+   * For instance, if we want to implement 'map+1' via 'reduce', as other real
+   * Functional Languages does:
+   *
+   *     Stream.reduce((acc, newval) => {
+   *       // To embed the new element into the Stream,
+   *       // via our observing-notifying interface.
+   *       return acc.notify(newval + 1);
+   *       // aka. acc ++ Stream(newval + 1)
+   *       // aka. acc ++ [newval + 1]
+   *     }, new Stream());
+   *
+   * And since we don't have native lazy-evaluation, we can only use Promise
+   * to let caller now whether we get ended or not.
+   */
+  Stream.prototype.reduce = function(reducer, initValue) {
+    var promised = new Promise((resolve) => {
+      this.reducingDone = resolve;
+    });
+    this.reducer = reducer;
+    this.accumulatedValue = initValue;
+    return promised;
+  };
+
+  /**
+   * If this Stream is finite and it's now ended,
+   * call the close.
+   */
+  Stream.prototype.close = function() {
+    if (null !== this.reducingDone) {
+      this.reducingDone(this.accumulatedValue);
+    }
+    return this;
+  };
+
+  Stream.prototype.notify = function(value) {
+    // If there is a reducing continues:
+    if (null !== this.reducer) {
+      this.accumulatedValue = this.reducer(this.accumulatedValue, value);
+    } else {
+      this.accumulatedValue = value;
+    }
+    this.observers.forEach((observer) => {
+      observer.notify(value);
+    });
+    return this;
+  };
+
+  Stream.prototype.observe = function(observer) {
+    this.observers.push(observer);
+    return this;
+  };
+  exports.Stream = Stream;
+})(window);
+
