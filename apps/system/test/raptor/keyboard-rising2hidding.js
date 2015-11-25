518800a3fdac53add2cb196ccbf41788fa8455da
Greg Weng <snowmantw@gmail.com>
2015-11-25 21:32:59 +0800

    fix added test

--- apps/system/test/raptor/keyboard-rising2hidding.js
+++ apps/system/test/raptor/keyboard-rising2hidding.js
@@ -4,24 +4,14 @@ var Messages = require('../../../sms/test/marionette/lib/messages.js');
 var SETTINGS_APP = 'app://settings.gaiamobile.org';
 var SMS_APP = 'app://sms.gaiamobile.org';
 
-
 var actions = function(phase) {
   return phase.device.marionette
     .startSession()
     .then(function(client) {
-
-/*
-TODO: how to insert this into the pre-set client?
-
-      var client = marionette.client({
-        profile: {
-          prefs: {
-            'focusmanager.testmode': true
-          }
-        }
-      });
-*/
-
+      var Mgmt = require(__dirname +
+        '/../../../../tests/jsmarionette/plugins/marionette-apps/lib/mgmt.js');
+      var mgmt = new Mgmt(client);
+      mgmt.prepareClient();
       var getAppClass = function(app, region) {
         region = region || app;
         var AppClass = require(
@@ -49,41 +39,69 @@ TODO: how to insert this into the pre-set client?
       var quarterWidth, topHalf;
 
       var setup = function() {
+        var Loader = require(__dirname +
+          '/../../../../shared/test/integration/marionette_loader.js');
+        client.loader = new Loader(client);
+        console.error('>>>>>> client.apps: ', typeof client.apps)
+        //client.apps.mgmt.prepareClient();
+console.error('>>>>> setup 1');
         sys = getAppClass('system');
         //sys.waitForStartup();
+console.error('>>>>> setup 2');
         settings = sys.waitForLaunch(SETTINGS_APP);
+console.error('>>>>> setup 3');
         sms = sys.waitForLaunch(SMS_APP);
+console.error('>>>>> setup 4');
 
         smsLib = Messages.create(client);
+console.error('>>>>> setup 5');
 
         // Making sure the opening transition for the sms app is over.
         client.waitFor(function() {
           return sms.displayed() && !settings.displayed();
         });
+console.error('>>>>> setup 6');
 
         var width = client.executeScript(function() {
           return window.innerWidth;
         });
+console.error('>>>>> setup 7');
         quarterWidth = width / 4;
+console.error('>>>>> setup 8');
 
         var height = client.executeScript(function() {
           return window.innerHeight;
         });
+console.error('>>>>> setup 9');
         topHalf = height / 2 - 100;
       };
+console.error('>>>>> setup');
       setup();
+console.error('>>>>> setup done');
 
+console.error('>>>>> switchframe (sms)');
       // Focusing the keyboard in the sms app
       client.switchToFrame(sms);
+console.error('>>>>> switchframe (sms) done');
+console.error('>>>>> initialHeight');
       var initialHeight = client.executeScript(function() {
         return window.wrappedJSObject.innerHeight;
       });
+console.error('>>>>> initialHeight done');
+console.error('>>>>> navigateToComposer');
       smsLib.Inbox.navigateToComposer();
+console.error('>>>>> navigateToComposer done');
+console.error('>>>>> top()');
       var composer = smsLib.Composer;
       composer.messageInput.tap();
+console.error('>>>>> top() done');
 
+console.error('>>>>> switchToFrame ');
       client.switchToFrame();
+console.error('>>>>> switchToFrame done');
+console.error('>>>>> waitForKeyboard');
       sys.waitForKeyboard();
+console.error('>>>>> waitForKeyboard done');
 
       deferred.resolve();
       return deferred.promise;
